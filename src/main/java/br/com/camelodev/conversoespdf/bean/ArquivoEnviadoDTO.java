2bd989cb43fd25c1b423e93c4df080411577922f
alexandre ferreira <alexandrecamelo@gmail.com>
2024-08-04 14:24:25 -0300

    Ajuste application.properties

--- src/main/java/br/com/camelodev/conversoespdf/bean/ArquivoEnviadoDTO.java
+++ src/main/java/br/com/camelodev/conversoespdf/bean/ArquivoEnviadoDTO.java
@@ -1,25 +1,20 @@
- package br.com.camelodev.conversoespdf.bean;
+package br.com.camelodev.conversoespdf.bean;
 
 import java.io.FileOutputStream;
 
 import org.primefaces.model.StreamedContent;
 import org.primefaces.model.file.UploadedFile;
 
-
 public class ArquivoEnviadoDTO {
-
- public static String nomeArquivoEnviado;
- public static String nomeArquivoEnviadoParaBaixar;
- public static String caminhoCompletoArquivoEnviado;
- public static String pastaOndeColocarImagens;
- public static UploadedFile uplArquivoEnviado;
- public static boolean pararProcesso = false;
- public static StreamedContent arquivoParaBaixar;
- public static FileOutputStream arquivoStream;
- public static int totalCaracteresArquivoGerado;
- public static boolean palavraLongaTxtPdf = false;	
- public static boolean umArquivoPorFoto = false;	
-	
-	
-	
+	public static String nomeArquivoEnviado;
+	public static String nomeArquivoEnviadoParaBaixar;
+	public static String caminhoCompletoArquivoEnviado;
+	public static String pastaOndeColocarImagens;
+	public static UploadedFile uplArquivoEnviado;
+	public static boolean pararProcesso = false;
+	public static StreamedContent arquivoParaBaixar;
+	public static FileOutputStream arquivoStream;
+	public static int totalCaracteresArquivoGerado;
+	public static boolean palavraLongaTxtPdf = false;
+	public static boolean umArquivoPorFoto = false;
 }
